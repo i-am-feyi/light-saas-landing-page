bdde43d09f10953f26ba0c629a38343c1911dc2b
Feyisayo Badmus <feymus99@gmail.com>
2024-07-26 15:49:31 +0300

    Implement LogoTicker Animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import acmeLogo from "@/assets/logo-acme.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
@@ -5,20 +7,39 @@ import celestialLogo from "@/assets/logo-celestial.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import apexLogo from "@/assets/logo-apex.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const Logos = () => (
+  <>
+    <Image src={acmeLogo} alt="Acme logo" />
+    <Image src={quantumLogo} alt="Quantum logo" />
+    <Image src={echoLogo} alt="Echo logo" />
+    <Image src={celestialLogo} alt="Celestial logo" />
+    <Image src={pulseLogo} alt="Pulse logo" />
+    <Image src={apexLogo} alt="Apex logo" />
+  </>
+);
 
 export const LogoTicker = () => {
   return (
     <section className="bg-white py-8 md:py-12">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none *:logo-ticker-image">
-            <Image src={acmeLogo} alt="Acme logo" />
-            <Image src={quantumLogo} alt="Quantum logo" />
-            <Image src={echoLogo} alt="Echo logo" />
-            <Image src={celestialLogo} alt="Celestial logo" />
-            <Image src={pulseLogo} alt="Pulse logo" />
-            <Image src={apexLogo} alt="Apex logo" />
-          </div>
+          <motion.div
+            className="flex gap-14 flex-none *:logo-ticker-image pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
+            <Logos />
+            <Logos />
+          </motion.div>
         </div>
       </div>
     </section>
