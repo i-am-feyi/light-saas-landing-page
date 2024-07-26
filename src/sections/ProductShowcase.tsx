fb16e1da0d3f7582b8d7206874ff86cc973a7b2c
Feyisayo Badmus <feymus99@gmail.com>
2024-07-26 15:52:44 +0300

    Implement Product Showcase Animation

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import Image from "next/image";
+import { useRef } from "react";
 import TubeImage from "@/assets/tube.png";
 import PyramidImage from "@/assets/pyramid.png";
 import ProductImage from "@/assets/product-image.png";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [-150, 150]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading">
           <div className="flex justify-center">
@@ -19,18 +35,25 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={ProductImage} alt="Product showcase" className="mt-10" />
-          <Image
-            src={PyramidImage}
+          <motion.img
+            src={PyramidImage.src}
             alt="Pyramid"
             className="hidden md:block absolute -right-36 -top-32"
             height={262}
             width={262}
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={TubeImage}
+          <motion.img
+            src={TubeImage.src}
             alt="Tube"
             className="hidden md:block absolute -left-36 bottom-24"
             height={248}
+            width={248}
+            style={{
+              translateY,
+            }}
           />
         </div>
       </div>
