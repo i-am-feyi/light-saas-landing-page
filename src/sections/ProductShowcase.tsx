cf32559656f7b9ab3a0243ea56778897f1dc6b34
Feyisayo Badmus <feymus99@gmail.com>
2024-07-26 10:07:53 +0300

    Implement Product Showcase

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,39 @@
+import Image from "next/image";
+import TubeImage from "@/assets/tube.png";
+import PyramidImage from "@/assets/pyramid.png";
+import ProductImage from "@/assets/product-image.png";
+
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="section-title mt-5">A more effective way to track progress</h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e] mt-5">
+            Effortlessly turn your ideas into a fully functional, responsive, SaaS website
+            in just minutes with this template.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={ProductImage} alt="Product showcase" className="mt-10" />
+          <Image
+            src={PyramidImage}
+            alt="Pyramid"
+            className="hidden md:block absolute -right-36 -top-32"
+            height={262}
+            width={262}
+          />
+          <Image
+            src={TubeImage}
+            alt="Tube"
+            className="hidden md:block absolute -left-36 bottom-24"
+            height={248}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
