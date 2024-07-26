f98558a266edcea72f57f0f5cfb32ff6f6afe9f3
Feyisayo Badmus <feymus99@gmail.com>
2024-07-26 10:35:19 +0300

    Implement Pricing Section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -7,12 +7,12 @@ export const ProductShowcase = () => {
   return (
     <section className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip">
       <div className="container">
-        <div className="max-w-[540px] mx-auto">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Boost your productivity</div>
           </div>
           <h2 className="section-title mt-5">A more effective way to track progress</h2>
-          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e] mt-5">
+          <p className="section-text mt-5">
             Effortlessly turn your ideas into a fully functional, responsive, SaaS website
             in just minutes with this template.
           </p>
